6f3dcb52170c8830d6c78dea2aceae4109473f65
Raul Vega <raul.vega.sge@mango.com>
2019-02-20 17:53:29 +0100

    working on minimum node server

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import cors from 'cors';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(cors());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.html');
 });
 
 app.listen(3000, () =>
