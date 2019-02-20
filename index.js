ba5148c995c3fe32b620a205af5d23d5eb59acfa
Raul Vega <raul.vega.sge@mango.com>
2019-02-20 17:02:33 +0100

    Testing input instead of stream of video

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
-// Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger");
 
-// Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
@@ -21,15 +18,13 @@ function cameraStart() {
         });
 }
 
-// Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-    // track.stop();
+    track.stop();
 };
 
-// Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
